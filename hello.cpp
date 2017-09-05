eed4feae906ab86271f07d258318bc2589a547cd
Kanguri <kanguri@kanguri.me>
2017-09-05 11:35:09 -0400

    command line arguments used for input

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 float interestingFunction(int a, int b)
 {
@@ -7,13 +8,27 @@ float interestingFunction(int a, int b)
 	return r;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc < 3)
+	{
+		std::cout << "Usage: " << argv[0] << " a b\n";
+		return 0;
+	}
+	std::istringstream ss(argv[1]);
+	std::istringstream ss2(argv[2]);
 	int a;
 	int b;
-	std::cout << "Please enter the first integer: ";
-	std::cin >> a;
-	std::cout << "Please enter the second integer: ";
-	std::cin >> b;
+	if (!(ss >> a))
+	{
+		std::cerr << "Invalid number " << argv[1] << "\n";
+		return 1;
+	}
+	if (!(ss2 >> b))
+	{
+		std::cerr << "Invalid number " << argv[2] << "\n";
+		return 1;
+	}
+	
 	std::cout << "Here is the result: " << interestingFunction(a, b) << "\n";
 }
